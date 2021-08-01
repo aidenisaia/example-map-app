b3eb4e80049d55cc70d3ed4b53bf135147c3f4eb
Aiden Isaia <aidenisaia@gmail.com>
2021-08-01 12:54:28 -0700

    Trying out mapbox

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,77 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>{{ message }}</h1>
+     <!-- <button v-on:click="doMapbox()">Do Mapbox</button> -->
+    <div id='map' style='width: 400px; height: 300px;'></div>
   </div>
 </template>
 
+<style>
+#marker {
+  background-image: url("https://docs.mapbox.com/mapbox-gl-js/assets/washington-monument.jpg");
+  background-size: cover;
+  width: 50px;
+  height: 50px;
+  border-radius: 50%;
+  cursor: pointer;
+}
+
+.mapboxgl-popup {
+  max-width: 200px;
+}
+</style>
+
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import mapboxgl from "mapbox-gl"; // or "const mapboxgl = require('mapbox-gl');"
 
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+  data: function () {
+    return {
+      message: "Welcome to Vue.js!",
+      places: [
+        { lat: -25.363, lng: 131.044, description: "A place in Australia" },
+        { lat: -33.8675, lng: 151.207, description: "The main city!" },
+      ],
+    };
+  },
+  mounted: function () {
+    this.doMapbox();
+  },
+  methods: {
+    doMapbox: function () {
+      mapboxgl.accessToken = "";
+      var monument = [-77.0353, 38.8895];
+      // console.log(process.env.VUE_APP_MAPBOX_API_KEY)
+      const map = new mapboxgl.Map({
+        container: "map", // container ID
+        style: "mapbox://styles/mapbox/satellite-streets-v11", // style URL
+        center: monument,
+        zoom: 5,
+      });
+      // create the popup
+      var popup = new mapboxgl.Popup({ offset: 25 }).setText("Construction on the Washington Monument began in 1848.");
+
+      // create DOM element for the marker
+      var el = document.createElement("div");
+      el.id = "marker";
+
+      // create the marker
+      new mapboxgl.Marker(el)
+        .setLngLat(monument)
+        .setPopup(popup) // sets a popup on this marker
+        .addTo(map);
+
+      // loop through places and make a new marker for each element
+      this.places.forEach(function (place) {
+        console.log(place);
+        // create the popup
+        var popup = new mapboxgl.Popup({ offset: 25 }).setText(place.description);
+        new mapboxgl.Marker().setLngLat([place.lng, place.lat]).setPopup(popup).addTo(map);
+      });
+
+      // Create a default Marker, colored black, rotated 45 degrees.
+      var marker2 = new mapboxgl.Marker({ color: "black", rotation: 45 }).setLngLat([12.65147, 55.608166]).addTo(map);
+    },
+  },
+};
 </script>
